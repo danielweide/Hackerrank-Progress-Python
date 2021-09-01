4a1c440a0e88e631c6b006690b45e0276739f1c7
DanielWeide <danielkwang@outlook.com>
2021-09-01 17:30:17 +0800

    Intro - Division

---
+++ src/Practice/Intro-division.py
@@ -0,0 +1,14 @@
+'''
+Created on 1 Sep 2021
+
+@author: dankmint
+'''
+
+def division(a,b):
+    print(a//b)
+    print(a/b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    division(a,b)
